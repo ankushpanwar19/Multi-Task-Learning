e40c3cb2527526c2615bea64bf4a3e9caee061d8
Abhinav <abhinavaggrwal@gmail.com>
2020-03-09 13:01:34 +0100

    adding clean prob1 and prob2a code

--- project_01/prob1.py
+++ project_01/prob1.py
@@ -1,4 +1,3 @@
-#%%
 import os
 import numpy as np
 import pandas as pd
@@ -8,37 +7,46 @@ import cv2
 import load_data
 import utils
 
-#%%
-fpath_seg = 'data/segmentation_data.p'
 
+def discretize_pts(velo_pts, bin_resolution):
+    '''
+    It returns disretized (binned) points with given
+    bin resolution
+    '''
+
+    min_val = velo_pts.min()
+    max_val = velo_pts.max()
+
+    val_range = np.arange(min_val, max_val, bin_resolution)
+    discrete_pts = np.digitize(velo_pts, val_range).astype(int)
+
+    return discrete_pts
+
+## Loading segmentation data
+fpath_seg = 'data/segmentation_data.p'
 seg_data = load_data.load_data(fpath_seg)
 
+velo_pts = seg_data['velodyne']
 
-x_min = seg_data['velodyne'][:,0].min()
-x_max = seg_data['velodyne'][:,0].max()
 
-y_min = seg_data['velodyne'][:,1].min()
-y_max = seg_data['velodyne'][:,1].max()
+## Discretizing points in all three directions
+x_disc = discretize_pts(velo_pts[:,0], 0.2)
+y_disc = discretize_pts(velo_pts[:,1], 0.2)
+z_disc = discretize_pts(velo_pts[:,2], 0.3)
 
-z_min = seg_data['velodyne'][:,2].min()
-z_max = seg_data['velodyne'][:,2].max()
 
-x_range = np.arange(x_min, x_max, 0.2)
-y_range = np.arange(y_min, y_max, 0.2)
-z_range = np.arange(z_min, z_max, 0.3)
-
-x_disc = np.digitize(seg_data['velodyne'][:,0], x_range).astype(int)
-y_disc = np.digitize(seg_data['velodyne'][:,1], y_range).astype(int)
-z_disc = np.digitize(seg_data['velodyne'][:,2], z_range).astype(int)
-
-disc_array = np.array([x_disc, y_disc, z_disc, seg_data['velodyne'][:,3]]).T
+## Grouping points in same bin to max intensity value
+disc_array = np.array([x_disc, y_disc, z_disc, velo_pts[:,3]]).T
 df = pd.DataFrame(disc_array, columns=['x','y','z','intensity'])
 
-bin_intensity = df.groupby(['x','y','z'])['intensity'].max()
+bin_intensity = df.groupby(['x','y','z'])['intensity'].max().reset_index()
 
-bin_intensity = bin_intensity.reset_index()
+
+## Grouping points with same x and y coordinate to to max intensity value
 bin_intensity_xy = bin_intensity.groupby(['x','y'])['intensity'].max().reset_index()
 
+
+## Creating BEV image 
 img = bin_intensity_xy.values
 im=np.zeros((int(img[:,0].max()),int(img[:,1].max())))
 
